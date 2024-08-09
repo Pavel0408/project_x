e4e0789eaba07494073c292f8f35266838390784
Pavel Grigorev <grigorev-pv@sber-zvuk.com>
2024-08-09 10:24:14 +0300

    added core prisma libraries

--- shared/manager/models/src/lib/shared-manager-models.spec.ts
+++
@@ -1,7 +0,0 @@
-import { sharedManagerModels } from './shared-manager-models';
-
-describe('sharedManagerModels', () => {
-  it('should work', () => {
-    expect(sharedManagerModels()).toEqual('shared-manager-models');
-  });
-});
